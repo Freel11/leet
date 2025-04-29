c2535200480a1aa8efc88554fbd94926f033a218
Freel11 <jadefreeljd@gmail.com>
2025-04-29 10:13:04 -0400

    Two sum completed

---
+++ problems/1.twoSum/injex.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) continue;
+      if (nums[i] + nums[j] === target) {
+        console.log([i, j]);
+        return [i, j];
+      }
+    }
+  }
+};
+
+twoSum([2, 7, 11, 15], 9);
+twoSum([3, 2, 4], 6);
+twoSum([3, 3], 6);
